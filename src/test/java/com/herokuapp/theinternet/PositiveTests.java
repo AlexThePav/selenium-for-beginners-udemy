6ff313473ab3f6fd85b8b3fbc3b6d98146185e5c
Mac Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2019-11-01 17:28:02 +0200

    firster commit

---
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -0,0 +1,56 @@
+package com.herokuapp.theinternet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class PositiveTests {
+
+	@Test
+	public void loginTest() {
+		System.out.println("Starting loginTest...");
+		
+//		Create driver
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+		WebDriver driver = new ChromeDriver();
+
+//		sleep for 3 seconds
+		sleep(3000);
+
+//		maximize browser window
+		driver.manage().window().maximize();
+
+//		open test page
+		String url = "http://the-internet.herokuapp.com/secure";
+		driver.get(url);
+		System.out.println("Page is opened");
+		
+//		sleep for 3 seconds
+		sleep(3000);
+
+//		enter username
+
+//		enter password
+
+//		click login button
+
+//		verifications:
+//		new url
+//		logout button visible
+//		successful login message
+		
+		
+//		close browser
+		driver.quit();
+	}
+
+	private void sleep(long m) {
+		try {
+			Thread.sleep(m);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
